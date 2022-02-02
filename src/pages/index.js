5309a3b685e29e6b8bd3a25c81d6e72669c96b8c
ericYager <eyager@amazon.com>
2022-02-02 16:12:53 -0600

    adding aws-amplify dependency

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import Carousel from '../components/homepage/carousel'
+import Amplify from 'aws-amplify'
+import config from '../aws-exports'
+Amplify.configure(config)
 
 
 
