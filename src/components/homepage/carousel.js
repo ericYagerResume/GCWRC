258c30b32e726953e5629f2946ed9514ae781140
ericYager <eyager@amazon.com>
2022-02-04 14:57:10 -0600

    testing image processing using canned gatsby images

--- src/components/homepage/carousel.js
+++ src/components/homepage/carousel.js
@@ -5,7 +5,7 @@ function Carousel(){
     return(
         <div className="flex flex-wrap justify-center">
             <div className="w-1/12 sm:w-4/12 px-1">
-                <StaticImage src={"../../images/homepageImage.jpg"} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                <StaticImage src={"../../images/gatsby-astronaut.png"} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
             </div>
         </div>
 )}
